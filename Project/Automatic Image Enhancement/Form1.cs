870616358d7b19f4038003ec32b37522f94f928d
Vlad David <vladdavidapps@gmail.com>
2020-01-07 10:28:58 +0200

    finished and sent project

--- Project/Automatic Image Enhancement/Form1.cs
+++ Project/Automatic Image Enhancement/Form1.cs
@@ -30,13 +30,13 @@ namespace Automatic_Image_Enhancement
 
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.menuStrip2 = new System.Windows.Forms.MenuStrip();
@@ -55,132 +55,132 @@ namespace Automatic_Image_Enhancement
             this.menuStrip2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.HistogramChart)).BeginInit();
             this.SuspendLayout();
-            //
+            // 
             // pictureBox2
-            //
+            // 
             this.pictureBox2.Image = global::Automatic_Image_Enhancement.Properties.Resources.dog800x600blurry;
-            this.pictureBox2.Location = new System.Drawing.Point(884, 27);
+            this.pictureBox2.Location = new System.Drawing.Point(784, 27);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(800, 822);
+            this.pictureBox2.Size = new System.Drawing.Size(700, 819);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
-            //
+            // 
             // pictureBox1
-            //
+            // 
             this.pictureBox1.Image = global::Automatic_Image_Enhancement.Properties.Resources.dog800x600;
             this.pictureBox1.Location = new System.Drawing.Point(12, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(800, 822);
+            this.pictureBox1.Size = new System.Drawing.Size(700, 822);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            //
+            // 
             // menuStrip2
-            //
+            // 
             this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
             this.editToolStripMenuItem});
             this.menuStrip2.Location = new System.Drawing.Point(0, 0);
             this.menuStrip2.Name = "menuStrip2";
-            this.menuStrip2.Size = new System.Drawing.Size(2002, 24);
+            this.menuStrip2.Size = new System.Drawing.Size(1807, 24);
             this.menuStrip2.TabIndex = 4;
             this.menuStrip2.Text = "menuStrip2";
-            //
+            // 
             // fileToolStripMenuItem
-            //
+            // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
             this.saveToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
-            //
+            // 
             // openToolStripMenuItem
-            //
+            // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-            //
+            // 
             // saveToolStripMenuItem
-            //
+            // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
-            //
+            // 
             // editToolStripMenuItem
-            //
+            // 
             this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.makeGrayscaleToolStripMenuItem,
             this.automaticAdjustmentToolStripMenuItem});
             this.editToolStripMenuItem.Name = "editToolStripMenuItem";
             this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
             this.editToolStripMenuItem.Text = "Edit";
-            //
+            // 
             // makeGrayscaleToolStripMenuItem
-            //
+            // 
             this.makeGrayscaleToolStripMenuItem.Name = "makeGrayscaleToolStripMenuItem";
             this.makeGrayscaleToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
             this.makeGrayscaleToolStripMenuItem.Text = "Make grayscale";
             this.makeGrayscaleToolStripMenuItem.Click += new System.EventHandler(this.makeGrayscaleToolStripMenuItem_Click);
-            //
+            // 
             // automaticAdjustmentToolStripMenuItem
-            //
+            // 
             this.automaticAdjustmentToolStripMenuItem.Name = "automaticAdjustmentToolStripMenuItem";
             this.automaticAdjustmentToolStripMenuItem.Size = new System.Drawing.Size(206, 22);
             this.automaticAdjustmentToolStripMenuItem.Text = "Automatic Enhancement";
             this.automaticAdjustmentToolStripMenuItem.Click += new System.EventHandler(this.automaticEnhancementToolStripMenuItem_Click);
-            //
+            // 
             // openFileDialog1
-            //
+            // 
             this.openFileDialog1.FileName = "openFileDialog1";
-            //
+            // 
             // swapButton
-            //
-            this.swapButton.Location = new System.Drawing.Point(818, 420);
+            // 
+            this.swapButton.Location = new System.Drawing.Point(718, 424);
             this.swapButton.Name = "swapButton";
             this.swapButton.Size = new System.Drawing.Size(60, 60);
             this.swapButton.TabIndex = 5;
             this.swapButton.Text = "Swap";
             this.swapButton.UseVisualStyleBackColor = true;
             this.swapButton.Click += new System.EventHandler(this.swapButon_Click_1);
-            //
+            // 
             // HistogramChart
-            //
-            chartArea1.Name = "ChartArea1";
-            chartArea2.Name = "ChartArea2";
-            chartArea3.Name = "ChartArea3";
-            this.HistogramChart.ChartAreas.Add(chartArea1);
-            this.HistogramChart.ChartAreas.Add(chartArea2);
-            this.HistogramChart.ChartAreas.Add(chartArea3);
-            legend1.Name = "Legend1";
-            this.HistogramChart.Legends.Add(legend1);
-            this.HistogramChart.Location = new System.Drawing.Point(1690, 27);
+            // 
+            chartArea4.Name = "ChartArea1";
+            chartArea5.Name = "ChartArea2";
+            chartArea6.Name = "ChartArea3";
+            this.HistogramChart.ChartAreas.Add(chartArea4);
+            this.HistogramChart.ChartAreas.Add(chartArea5);
+            this.HistogramChart.ChartAreas.Add(chartArea6);
+            legend2.Name = "Legend1";
+            this.HistogramChart.Legends.Add(legend2);
+            this.HistogramChart.Location = new System.Drawing.Point(1490, 27);
             this.HistogramChart.Name = "HistogramChart";
-            series1.ChartArea = "ChartArea1";
-            series1.Color = System.Drawing.Color.Red;
-            series1.Legend = "Legend1";
-            series1.Name = "Red";
-            series2.ChartArea = "ChartArea2";
-            series2.Color = System.Drawing.Color.Green;
-            series2.Legend = "Legend1";
-            series2.Name = "Green";
-            series3.ChartArea = "ChartArea3";
-            series3.Color = System.Drawing.Color.Blue;
-            series3.Legend = "Legend1";
-            series3.Name = "Blue";
-            this.HistogramChart.Series.Add(series1);
-            this.HistogramChart.Series.Add(series2);
-            this.HistogramChart.Series.Add(series3);
+            series4.ChartArea = "ChartArea1";
+            series4.Color = System.Drawing.Color.Red;
+            series4.Legend = "Legend1";
+            series4.Name = "Red";
+            series5.ChartArea = "ChartArea2";
+            series5.Color = System.Drawing.Color.Green;
+            series5.Legend = "Legend1";
+            series5.Name = "Green";
+            series6.ChartArea = "ChartArea3";
+            series6.Color = System.Drawing.Color.Blue;
+            series6.Legend = "Legend1";
+            series6.Name = "Blue";
+            this.HistogramChart.Series.Add(series4);
+            this.HistogramChart.Series.Add(series5);
+            this.HistogramChart.Series.Add(series6);
             this.HistogramChart.Size = new System.Drawing.Size(300, 822);
             this.HistogramChart.TabIndex = 6;
             this.HistogramChart.Text = "chart1";
-            //
+            // 
             // Form1
-            //
-            this.ClientSize = new System.Drawing.Size(2002, 861);
+            // 
+            this.ClientSize = new System.Drawing.Size(1807, 858);
             this.Controls.Add(this.HistogramChart);
             this.Controls.Add(this.swapButton);
             this.Controls.Add(this.pictureBox2);
@@ -196,6 +196,7 @@ namespace Automatic_Image_Enhancement
             ((System.ComponentModel.ISupportInitialize)(this.HistogramChart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
